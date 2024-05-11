4753c23d2dfa26c11f68e008552ebdc244e8776f
adelhussein <aadelshabib67009@gmail.com>
2024-05-11 03:08:55 +0300

    best seller list view item part one

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+import '../../../../../core/utils/assets.dart';
 import 'custom_app_bar.dart';
 import 'featured_list_view.dart';
 
@@ -15,9 +16,46 @@ class HomeViewBody extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: 20),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [CustomAppBar(), FeaturedBooksListView(),SizedBox(height: 50,),Text('Best Seller',style: Styles.titleMedium,)],
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            'Best Seller',
+            style: Styles.titleMedium,
+          ),
+          BestSellerListViewItem()
+        ],
       ),
     );
   }
 }
 
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .16,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5/ 4,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(5),
+                image: const DecorationImage(
+                    image: AssetImage(AssetsData.testImage), fit: BoxFit.fill),
+              ),
+            ),
+          ),
+          const Column()
+        ],
+      ),
+    );
+  }
+}
