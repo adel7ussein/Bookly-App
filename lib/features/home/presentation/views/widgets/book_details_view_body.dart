aa9a1c4c161920dc9a5e8a7c04d2e364d123faae
adelhussein <aadelshabib67009@gmail.com>
2024-05-13 22:54:37 +0300

    create books action

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -3,7 +3,9 @@ import 'package:bookly_app/features/home/presentation/views/widgets/booking_rati
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
+import 'books_action.dart';
 import 'custom_book_details_app_bar.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -11,31 +13,44 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width ;
-    return  Padding(
+    var width = MediaQuery.of(context).size.width;
+    return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(children: [
-        const CustomBookDetailsAppBar(),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: width * .2),
-          child: const CustomBookImage(),
-        ),
-        const SizedBox(height: 43,),
-         Text('The Jungle Book',style:Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),),
-        const SizedBox(height: 6,),
-
-         Opacity(
-           opacity: .7,
-           child: Text('Rudyard Kipling',style: Styles.textStyle18.copyWith(
-            fontStyle: FontStyle.italic,
-             fontWeight: FontWeight.w500
-                   ),),
-         ),
-        const SizedBox(height: 18,),
-        BookRating(mainAxisAlignment: MainAxisAlignment.center,),
-        
-      ],),
+      child: Column(
+        children: [
+          const CustomBookDetailsAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * .2),
+            child: const CustomBookImage(),
+          ),
+          const SizedBox(
+            height: 43,
+          ),
+          Text(
+            'The Jungle Book',
+            style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Styles.textStyle18.copyWith(
+                  fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+            ),
+          ),
+          const SizedBox(
+            height: 18,
+          ),
+          BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+          SizedBox(height: 37,),
+          BooksAction()
+        ],
+      ),
     );
   }
 }
-
---
+++ lib/features/home/presentation/views/widgets/books_action.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/widgets/custom_button.dart';
+
+class BooksAction extends StatelessWidget {
+  const BooksAction({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        children: [
+          Expanded(
+              child: CustomButton(
+            background: Colors.white,
+            textColor: Colors.black,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(16), bottomLeft: Radius.circular(16)), text: '19.99€',
+          )),
+          Expanded(
+              child: CustomButton(
+            background: Color(0xffEF8262),
+            textColor: Colors.white,
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(16), bottomRight: Radius.circular(16)), text: 'Free preview', fontSize:16,
+          ))
+        ],
+      ),
+    );
+  }
+}
