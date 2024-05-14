5959b277915b4040bd7f1e7fde5b8114341e7973
adelhussein <aadelshabib67009@gmail.com>
2024-05-14 22:08:24 +0300

    create search view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,14 +1,9 @@
-import 'package:bookly_app/core/utils/styles.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/booking_rating.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/similar_books_list_view.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/similar_books_section.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 import 'book_details_section.dart';
-import 'books_action.dart';
 import 'custom_book_details_app_bar.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
