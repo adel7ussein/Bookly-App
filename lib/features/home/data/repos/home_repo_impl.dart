8b9b582528b2bcd7a8620f67c84dcdd0ffe75b28
adelhussein <aadelshabib67009@gmail.com>
2024-06-12 04:58:55 +0300

    refactor home repo impl

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -3,20 +3,23 @@ import 'package:bookly_app/features/home/data/data_sources/home_local_data_sourc
 import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+
 class HomeRepoImpl extends HomeRepo {
   final HomeRemoteDataSource homeRemoteDataSource;
   final HomeRemoteDataSource homLocalDataSource;
 
-  HomeRepoImpl({required this.homeRemoteDataSource, required this.homLocalDataSource});
+  HomeRepoImpl(
+      {required this.homeRemoteDataSource, required this.homLocalDataSource});
 
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks()async{
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
     try {
-      var booksList = homLocalDataSource.fetchFeaturedBooks();
-      if(booksList.isNotEmpty){
-        return right(booksList);
+      List<BookEntity> books;
+      books = homLocalDataSource.fetchFeaturedBooks();
+      if (books.isNotEmpty) {
+        return right(books);
       }
-      var books = homeRemoteDataSource.fetchFeaturedBooks();
+      books = homeRemoteDataSource.fetchFeaturedBooks();
       return right(books);
     } on Exception catch (e) {
       return left(Failure());
@@ -24,17 +27,17 @@ class HomeRepoImpl extends HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async{
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async {
     try {
-      var booksList = homLocalDataSource.fetchNewestBooks();
-      if(booksList.isNotEmpty){
-        return right(booksList);
+      List<BookEntity> books;
+      books = homLocalDataSource.fetchNewestBooks();
+      if (books.isNotEmpty) {
+        return right(books);
       }
-      var books = homeRemoteDataSource.fetchNewestBooks();
+      books = homeRemoteDataSource.fetchNewestBooks();
       return right(books);
     } on Exception catch (e) {
       return left(Failure());
     }
   }
-
 }
