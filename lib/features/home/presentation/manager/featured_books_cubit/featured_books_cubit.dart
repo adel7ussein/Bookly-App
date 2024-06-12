82b8e24b37071ec3d6c4d6dec53e88a088a0810e
adelhussein <aadelshabib67009@gmail.com>
2024-06-12 20:41:48 +0300

    create featured books states

---
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../domain/entities/book_entity.dart';
+
+part 'featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+}
